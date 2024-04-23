3487a3e358024e487ebf9fb984c970d019d6ebfa
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2024-04-23 14:08:17 +0300

    add readme file

--- app/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC, ButtonHTMLAttributes } from 'react';
-
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
-
-const Button: FC<ButtonProps> = ({ children, ...rest }) => {
-  return <button {...rest}>{children}</button>;
-};
-
-export default Button;
