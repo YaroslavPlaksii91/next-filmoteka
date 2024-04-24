a947640529d0f7b85be94bf54630e008a415ef58
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2024-04-24 18:23:27 +0300

    add common constants

---
+++ app/utils/constants.ts
@@ -0,0 +1,22 @@
+export const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/original';
+
+export const FETCH_OPTIONS = Object.freeze({
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+  },
+});
+
+export const ROUTES = Object.freeze({
+  MOVIES: '/movies',
+  SHOWS: '/shows',
+});
+
+export const API_ENDPOINTS = Object.freeze({
+  NOW_PLAYING_MOVIES: `${BASE_API_URL}movie/now_playing`,
+  POPULAR_MOVIES: `${BASE_API_URL}movie/popular`,
+  MOVIE_BY_ID: `${BASE_API_URL}movie`,
+});
