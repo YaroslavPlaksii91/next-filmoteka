e6f9ac899ade5d6b33c1dea1aa60ef511342ef84
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2024-04-24 18:24:18 +0300

    create dynamic page for each movie

---
+++ app/movies/[id]/page.tsx
@@ -0,0 +1,23 @@
+import MovieCard from '@/app/components/MovieCard/MovieCard';
+import { fetchMovie } from '@/app/services/fetchMovies';
+import { BASE_IMG_URL } from '@/app/utils/constants';
+import React from 'react';
+
+import type { MoviePageProps } from './types';
+
+const MoviePage: React.FC<MoviePageProps> = async ({ params: { id } }) => {
+  const movie = await fetchMovie(id);
+
+  return (
+    <div
+      className="absolute inset-0 bg-cover bg-center"
+      style={{
+        backgroundImage: `url('${BASE_IMG_URL}/${movie.backdrop_path}')`,
+      }}
+    >
+      <MovieCard movie={movie} />
+    </div>
+  );
+};
+
+export default MoviePage;
---
+++ app/movies/[id]/types.ts
@@ -0,0 +1,5 @@
+export interface MoviePageProps {
+  params: {
+    id: string;
+  };
+}
