ab9f2fa09cb139d023ee1bec085410b6e214e5d8
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2024-04-26 17:07:12 +0300

    create footer component

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-8">
+      <div className="container mx-auto">
+        <p className="text-sm text-center">
+          © 2024 Moviec. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
