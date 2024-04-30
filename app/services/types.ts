f7af71ac8ec6d654ed2eaeca15e9f8482e2e1067
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2024-04-30 15:55:13 +0300

    add new types

--- app/services/types.ts
+++ app/services/types.ts
@@ -1,6 +1,29 @@
 import type { Movie } from '../components/MovieCard/types';
 
+export enum MediaType {
+  All = 'all',
+  Movie = 'movie',
+  TV = 'tv',
+}
+
+export enum TrendingTime {
+  Day = 'day',
+  Week = 'week',
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface FetchTrendingProps {
+  mediaType: MediaType;
+  time: TrendingTime;
+}
 export interface FetchMoviesResponse {
   page: number;
   results: Movie[];
+}
+export interface FetchGenresResponse {
+  genres: Genre[];
 }
